f7f7979e8b0bd795c0850ae9dfa3e90e9f40da02
zhangguo <506430706@qq.com>
2018-11-05 18:01:34 +0800

    页面修改

--- src/views/Front/User/User.vue
+++ src/views/Front/User/User.vue
@@ -5,9 +5,68 @@
   <Layout>
     <section id="introduce">
       <Content class="contentPadding">
-        <h3>币势得</h3>
-        <p>币势得云算力平台是国内先进的云算力数字投资平台。我们拥有最专业的云算力解决方案:我们拥有强大的技术团队，确保云算力的稳定运行;我们会员的收益尤为显著!加入币势得会员,您 可以在币势得享受更多优质服务。
-        </p>
+        <Row type="flex" justify="center">
+          <Col>
+          <Card style="width:350px">
+            <p slot="title">
+              登录注册
+            </p>
+            <Tabs value="name1">
+              <TabPane label="标签一" name="name1">
+                <Form ref="formInline" :model="formInline" :rules="ruleInline">
+                  <FormItem prop="user">
+                    <Input type="text" v-model="formInline.user" placeholder="Username">
+                    <Icon type="ios-person-outline" slot="prepend"></Icon>
+                    </Input>
+                  </FormItem>
+                  <FormItem prop="password">
+                    <Input type="password" v-model="formInline.password" placeholder="Password">
+                    <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                    </Input>
+                  </FormItem>
+                  <FormItem>
+                    <Button type="primary">登录</Button>
+                  </FormItem>
+                </Form>
+              </TabPane>
+              <TabPane label="标签二" name="name2">
+                <Form ref="formInline" :model="formInline" :rules="ruleInline">
+                  <FormItem prop="user">
+                    <Input type="text" v-model="formInline.user" placeholder="Username">
+                    <Icon type="ios-person-outline" slot="prepend"></Icon>
+                    </Input>
+                  </FormItem>
+                  <FormItem prop="password">
+                    <Input type="password" v-model="formInline.password" placeholder="Password">
+                    <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                    </Input>
+                  </FormItem>
+                  <FormItem>
+                    <Button type="primary">登录</Button>
+                  </FormItem>
+                </Form>
+              </TabPane>
+              <TabPane label="标签三" name="name3">
+                <Form ref="formInline" :model="formInline" :rules="ruleInline">
+                  <FormItem prop="user">
+                    <Input type="text" v-model="formInline.user" placeholder="Username">
+                    <Icon type="ios-person-outline" slot="prepend"></Icon>
+                    </Input>
+                  </FormItem>
+                  <FormItem prop="password">
+                    <Input type="password" v-model="formInline.password" placeholder="Password">
+                    <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                    </Input>
+                  </FormItem>
+                  <FormItem>
+                    <Button type="primary">登录</Button>
+                  </FormItem>
+                </Form>
+              </TabPane>
+            </Tabs>
+          </Card>
+          </Col>
+        </Row>
       </Content>
     </section>
   </Layout>
@@ -25,14 +84,36 @@ import {
 export default {
   data() {
     return {
+      formInline: {
+        user: '',
+        password: ''
+      },
+      ruleInline: {
+        user: [{
+          required: true,
+          message: '币势得: 用户名不能为空!',
+          trigger: 'blur'
+        }],
+        password: [{
+            required: true,
+            message: '币势得: 密码不能为空!',
+            trigger: 'blur'
+          },
+          {
+            type: 'string',
+            min: 6,
+            message: '币势得: 密码长度不能低于6位!',
+            trigger: 'blur'
+          }
+        ]
+      }
     }
   },
   components: {
     Header,
     Footer,
   },
-  mounted() {
-  }
+  mounted() {}
 }
 </script>
 

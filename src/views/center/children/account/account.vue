fb4885d60cb4cadcd36daef11bd6024c8536cb80
zhangguo <506430706@qq.com>
2018-12-12 18:17:04 +0800

    修改

--- src/views/center/children/account/account.vue
+++ src/views/center/children/account/account.vue
@@ -5,70 +5,66 @@
     <Layout>
       <Tabs>
         <TabPane label="我的算力套餐" style="padding:0 1rem;">
-          <Row type="flex" :gutter="16">
-            <Col span="8" v-for="item in 3">
-            <Card class="order" dis-hover>
-              <div class="time">
-                <div class="left">
-                  <p>2018.9.21 10:16</p>
-                  <p>套餐开始时间</p>
-                </div>
-                <div class="img">
-                  <img src="../../../../assets/icons/account/eth.svg" width="100%">
-                </div>
-                <div class="left">
-                  <p>2018.9.21 10:16</p>
-                  <p>套餐结束时间</p>
-                </div>
-              </div>
-              <div class="power">
-                <div class="eth">
-                  ETH
-                </div>
-                <div class="price">
-                  <div class="text">
-                    总算力（MH/s）
+          <swiper :options="swiperOption" ref="mySwiper">
+            <swiper-slide v-for="item in 5">
+              <Col span="24">
+                <Card class="order" dis-hover>
+                  <div class="time">
+                    <div class="left">
+                      <p>2018.9.21 10:16</p>
+                      <p>套餐开始时间</p>
+                    </div>
+                    <div class="img">
+                      <img src="../../../../assets/icons/account/eth.svg" width="100%">
+                    </div>
+                    <div class="left">
+                      <p>2018.9.21 10:16</p>
+                      <p>套餐结束时间</p>
+                    </div>
                   </div>
-                  <div class="num">
-                    255
+                  <div class="power">
+                    <div class="eth">
+                      ETH
+                    </div>
+                    <div class="price">
+                      <div class="text">
+                        总算力（MH/s）
+                      </div>
+                      <div class="num">
+                        255
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </Card>
-            </Col>
-          </Row>
+                </Card>
+              </Col>
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+          </swiper>
         </TabPane>
         <TabPane label="我的优惠券" style="padding:0 1rem;">
-          <Row type="flex" :gutter="16">
-            <Col span="8" v-for="item in 3">
-            <Card class="order" dis-hover>
-              <div class="time">
-                <div class="left">
-                  <p>2018.9.21 10:16</p>
-                  <p>套餐开始时间</p>
-                </div>
-                <div class="">
-                </div>
-                <div class="left">
-                  <p>2018.9.21 10:16</p>
-                  <p>套餐结束时间</p>
-                </div>
-              </div>
-              <div class="power">
-                <div class="eth">
-                  ETH
-                </div>
-                <div class="">
-                  <div class="">
-                    总算力（MH/s）
+          <Row type="flex" >
+            <swiper :options="swiperOption" ref="mySwiper">
+              <swiper-slide v-for="(item,index) in coupon">
+                <Col span="24">
+                <Card class="coupon" :class="item.type=='day'?'day':'discount'" dis-hover>
+                  <h6>{{item.type=='day'?item.argument+'天算力套餐优惠':item.argument*10+'折套餐优惠'}}</h6>
+                  <div class="btn">
+                    <Button type="default" ghost>立即使用</Button>
                   </div>
-                  <div class="">
-                    255
+                  <div class="footer">
+                    <div class="left">
+                      <p>开始时间 : 2018.9.21 10:16</p>
+                      <p>到期时间 : 2018.9.21 10:16</p>
+                    </div>
+                    <div class="right">
+                      {{item.type=='day'?item.argument+'天券':item.argument*10+'折券'}}
+                    </div>
                   </div>
-                </div>
-              </div>
-            </Card>
-            </Col>
+                </Card>
+                </Col>
+              </swiper-slide>
+              <div class="swiper-pagination" slot="pagination"></div>
+            </swiper>
           </Row>
         </TabPane>
       </Tabs>
@@ -76,10 +72,10 @@
     <Layout>
       <Tabs>
         <TabPane label="提现记录" style="padding:0 1rem;">
-          <Table highlight-row ref="currentRowTable" :columns="columns3" :data="data1"></Table>
+          <Table highlight-row ref="currentRowTable" :columns="columns" :data="tableData"></Table>
         </TabPane>
         <TabPane label="登录记录" style="padding:0 1rem;">
-          <Table highlight-row ref="currentRowTable" :columns="columns3" :data="data1"></Table>
+          <Table highlight-row ref="currentRowTable" :columns="columns" :data="tableData"></Table>
         </TabPane>
       </Tabs>
     </Layout>
@@ -100,21 +96,23 @@ export default {
       username: this.$store.state.username,
       usertelphone: this.$store.state.usertelphone,
       swiperOption: {
-        centeredSlides: true,
+        slidesPerView: 3,
+        spaceBetween: 5,
+        freeMode: true,
         autoplay: {
-          delay: 2500,
+          delay: 5000,
           disableOnInteraction: false
         },
         pagination: {
           el: '.swiper-pagination',
-          clickable: true
+          type: 'fraction',
         },
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev'
         }
       },
-      columns3: [{
+      columns: [{
           type: 'index',
           width: 60,
           align: 'center'
@@ -132,7 +130,7 @@ export default {
           key: 'address'
         }
       ],
-      data1: [{
+      tableData: [{
           name: 'John Brown',
           age: 18,
           address: 'New York No. 1 Lake Park',
@@ -156,13 +154,33 @@ export default {
           address: 'Ottawa No. 2 Lake Park',
           date: '2016-10-04'
         }
+      ],
+      coupon: [ //优惠券信息
+        {
+          type: 'day',
+          argument: 30,
+        },
+        {
+          type: 'discount',
+          argument: 0.8,
+        },
+        {
+          type: 'discount',
+          argument: 0.6,
+        },
+        {
+          type: 'discount',
+          argument: 0.6,
+        },
       ]
     }
   },
   methods: {},
   components: {
     Centerheader, //个人中心导航栏
-    Headbar
+    Headbar,
+    swiper,
+    swiperSlide
   },
   mounted() {},
 }
